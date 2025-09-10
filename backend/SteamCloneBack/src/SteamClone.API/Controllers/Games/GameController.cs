a877845aeb047104ff3c54f50f3ed7e26749dffe
Andrii <andriylavrenyuk12@gmail.com>
2025-09-10 21:32:13 +0300

    feat: added more logic to associated users for api

--- backend/SteamCloneBack/src/SteamClone.API/Controllers/Games/GameController.cs
+++ backend/SteamCloneBack/src/SteamClone.API/Controllers/Games/GameController.cs
@@ -30,7 +30,7 @@ public class GameController(IGameService gameService)
         var response = await gameService.UpdateCoverImageAsync(gameId, coverImage, cancellationToken);
         return GetResult(response);
     }
-    
+
     [Authorize(Policy = Settings.Roles.AdminOrManager)]
     [HttpPatch("update-screenshots-images/{gameId}")]
     public async Task<IActionResult> UpdateImages([FromRoute] string gameId,
@@ -38,10 +38,11 @@ public class GameController(IGameService gameService)
         [FromForm] List<string> imagesToDelete,
         CancellationToken cancellationToken)
     {
-        var response = await gameService.UpdateScreenshotsImagesAsync(gameId, newImages, imagesToDelete, cancellationToken);
+        var response =
+            await gameService.UpdateScreenshotsImagesAsync(gameId, newImages, imagesToDelete, cancellationToken);
         return GetResult(response);
     }
-    
+
     [Authorize(Policy = Settings.Roles.AdminOrManager)]
     [HttpPatch("associate-user")]
     public async Task<IActionResult> AssociateUserAsync([FromQuery] string gameId, string userId,
@@ -68,15 +69,16 @@ public class GameController(IGameService gameService)
         var result = await gameService.GetByAssociatedUserAsync(token);
         return GetResult(result);
     }
-    
+
     [Authorize(Roles = Settings.Roles.AdminRole)]
     [HttpPatch("approve")]
-    public async Task<IActionResult> ApproveAsync([FromQuery] string id, bool isApproved = true, CancellationToken token = default)
+    public async Task<IActionResult> ApproveAsync([FromQuery] string id, bool isApproved = true,
+        CancellationToken token = default)
     {
         var result = await gameService.ApproveAsync(id, isApproved, token);
         return GetResult(result);
     }
-    
+
     [Authorize(Roles = Settings.Roles.AdminRole)]
     [HttpGet("get-without-approval")]
     public async Task<IActionResult> GetWithoutApprovalAsync(CancellationToken token = default)
@@ -84,7 +86,7 @@ public class GameController(IGameService gameService)
         var result = await gameService.GetWithoutApprovalAsync(token);
         return GetResult(result);
     }
-    
+
     [HttpPost("buy/{id}")]
     public async Task<IActionResult> BuyGameAsync([FromRoute] string id, CancellationToken token = default)
     {
@@ -96,6 +98,20 @@ public class GameController(IGameService gameService)
     public async Task<IActionResult> IsGameBoughtAsync([FromRoute] string gameId, CancellationToken token = default)
     {
         var result = await gameService.IsGameBoughtAsync(gameId, token);
+        return GetResult(result);
+    }
+
+    [HttpGet("get-associated-users/{gameId}")]
+    public async Task<IActionResult> GetAssociatedUsersAsync(string gameId, CancellationToken token = default)
+    {
+        var result = await gameService.GetAssociatedUsersAsync(gameId, token);
+        return GetResult(result);
+    }
+
+    [HttpGet("is-owner/{gameId}")]
+    public async Task<IActionResult> IsOwnerAsync([FromRoute] string gameId, CancellationToken token = default)
+    {
+        var result = await gameService.IsGameOwnerAsync(gameId, token);
         return GetResult(result);
     }
 }
--- backend/SteamCloneBack/src/SteamClone.BLL/Services/DeveloperAndPublisherService/IDeveloperAndPublisherService.cs
+++ backend/SteamCloneBack/src/SteamClone.BLL/Services/DeveloperAndPublisherService/IDeveloperAndPublisherService.cs
@@ -10,4 +10,6 @@ public interface IDeveloperAndPublisherService : IServiceCRUD<string, CreateDeve
     Task<ServiceResponse> GetByAssociatedUserAsync(CancellationToken token);
     Task<ServiceResponse> ApproveAsync(string id, bool isApproved, CancellationToken token);
     Task<ServiceResponse> GetWithoutApprovalAsync(CancellationToken token);
+    Task<ServiceResponse> GetAssociatedUsersAsync(string devAndPubId, CancellationToken token);
+    Task<ServiceResponse> IsDevOrPubOwnerAsync(string devAndPubId, CancellationToken token);
 }
